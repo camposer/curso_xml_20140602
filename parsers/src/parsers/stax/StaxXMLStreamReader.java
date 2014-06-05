05676ffae412e8f750fd697ecd2568bc48cc0eb6
Rodolfo Campos <camposer@gmail.com>
2014-06-05 10:25:51 +0200

    Ejemplo básico de StAX

---
+++ parsers/src/parsers/stax/StaxXMLStreamReader.java
@@ -0,0 +1,90 @@
+package parsers.stax;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+import parsers.model.Employee;
+ 
+/**
+ * @see http://www.journaldev.com/1226/java-stax-cursor-based-api-read-xml-example
+ * @author camposer
+ *
+ */
+public class StaxXMLStreamReader {
+     
+    private static boolean bName;
+    private static boolean bAge;
+    private static boolean bGender;
+    private static boolean bRole;
+ 
+    public static void main(String[] args) {
+        String fileName = "xml/employees.xml";
+        List<Employee> empList = parseXML(fileName);
+        for(Employee emp : empList){
+            System.out.println(emp.toString());
+        }
+    }
+ 
+    private static List<Employee> parseXML(String fileName) {
+        List<Employee> empList = new ArrayList<>();
+        Employee emp = null;
+        XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+        try {
+            XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(new FileInputStream(fileName));
+            int event = xmlStreamReader.getEventType();
+            while(true){
+                switch(event) {
+                case XMLStreamConstants.START_ELEMENT:
+                    if(xmlStreamReader.getLocalName().equals("Employee")){
+                        emp = new Employee();
+                        emp.setId(Integer.parseInt(xmlStreamReader.getAttributeValue(0)));
+                    }else if(xmlStreamReader.getLocalName().equals("name")){
+                        bName=true;
+                    }else if(xmlStreamReader.getLocalName().equals("age")){
+                        bAge=true;
+                    }else if(xmlStreamReader.getLocalName().equals("role")){
+                        bRole=true;
+                    }else if(xmlStreamReader.getLocalName().equals("gender")){
+                        bGender=true;
+                    }
+                    break;
+                case XMLStreamConstants.CHARACTERS:
+                    if(bName){
+                        emp.setName(xmlStreamReader.getText());
+                        bName=false;
+                    }else if(bAge){
+                        emp.setAge(Integer.parseInt(xmlStreamReader.getText()));
+                        bAge=false;
+                    }else if(bGender){
+                        emp.setGender(xmlStreamReader.getText());
+                        bGender=false;
+                    }else if(bRole){
+                        emp.setRole(xmlStreamReader.getText());
+                        bRole=false;
+                    }
+                    break;
+                case XMLStreamConstants.END_ELEMENT:
+                    if(xmlStreamReader.getLocalName().equals("Employee")){
+                        empList.add(emp);
+                    }
+                    break;
+                }
+                if (!xmlStreamReader.hasNext())
+                    break;
+ 
+              event = xmlStreamReader.next();
+            }
+             
+        } catch (FileNotFoundException | XMLStreamException e) {
+            e.printStackTrace();
+        }
+        return empList;
+    }
+}
